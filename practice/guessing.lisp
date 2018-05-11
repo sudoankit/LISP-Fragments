8dd3c23c4c0366a7bed27ded3fb7e17e3d70783f
Ankit Panda <ankitpanda1014@gmail.com>
2018-05-11 18:13:23 +0530

    guess the number

---
+++ practice/guessing.lisp
@@ -0,0 +1,24 @@
+
+;;; Code from Land of Lisp
+
+;;; ==============================
+
+(defparameter *small* 1)
+
+(defparameter *big* 100)
+
+(defun guess-my-number () 
+  (ash (+ *small* *big* ) -1 ))
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number))) 􏰂 
+  (guess-my-number))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number))) 􏰂 
+  (guess-my-number))
+
+(defun start-over ()
+    (defparameter *small* 1)
+    (defparameter *big* 100)
+    (guess-my-number))
